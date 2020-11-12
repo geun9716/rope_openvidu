63012fb24ff4dd0c0b7c175db400e9ee94d5f333
geun9716 <crintop@gmail.com>
2020-11-12 15:11:07 +0900

    fix Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     app.use(  
     createProxyMiddleware('/users',  {
     
-    target: 'http://localhost:5000/',
+    target: 'http://52.79.134.9:5000/',
     changeOrigin: true
     })
  );
